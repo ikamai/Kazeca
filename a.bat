b52e30b45e02460ca0d2b9f0ef6df56510056d84
intanperdana <intanperdan4@yahoo.com>
2013-07-31 11:07:57 -0700

    asas

---
+++ a.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "asas"
+git remote add origin ssh://git@bitbucket.org/lauraduit/01.kazeca.git
+git merge remotes/origin/master
+git pull origin master
+git push -u origin master
