48564fb9901094189c34810bc9f14bdae4ea73a3
rizoa <rizoamaskuero@gmail.com>
2013-08-01 01:09:08 +0700

    Komit on 2013-08-01 at 01:09:08 AM

--- a.bat
+++ a.bat
@@ -1,8 +1,7 @@
 git init
 git add .
-git commit -m "`date +'Komit on %F at %r'`"
+git commit -m "asas"
 git remote add origin ssh://git@bitbucket.org/lauraduit/01.kazeca.git
 git merge remotes/origin/master
 git pull origin master
 git push -u origin master
-
